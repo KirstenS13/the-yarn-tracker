3530c565d1420c2e28725ccc853849e4c917cd9a
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-09-14 16:17:42 -0400

    made it so server can serve static pages

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 // imports
 const express = require('express');
 const yarntypeRouter = require('./routers/yarntype_router');
+const path = require('path')
 
 // create server
 const server = express();
 const port = process.env.PORT || 3001;
 
+// tell express where to serve static pages from
+server.use(express.static(path.join(__dirname, 'build')));
+
 // use middleware here
 // middleware to handle incoming json data
 server.use(express.json());
+// middleware to allow two ports with CORS - set header for everything
 
 // use routes and routers here
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
 server.use(yarntypeRouter);
 server.get('/api', (req, res) => {
     res.send('Hello, this is the server')
