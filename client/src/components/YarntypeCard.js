62939ddd7f084a75f592153db145f633b02e99ee
Kirsten S <kirstenjoy@kbsymanzik.org>
2022-09-20 18:37:07 -0400

    rendered data to the screen

---
+++ client/src/components/YarntypeCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import YarntypeList from './YarntypeList';
+
+const YarntypeCard = props => {
+    return (
+        <>
+            <p>Name: {props.name}</p>
+            <p>Brand: {props.brand}</p>
+            <p>Weight Category: {props.cyc_weight_category}</p>
+            <p>Weight: {props.ounces} oz/{props.grams} g</p>
+            <p>Length: {props.yards} yds/ {props.meters} m</p>
+            <p>Notes: {props.notes}</p>
+        </>
+    )
+}
+
+export default YarntypeCard;
--- client/src/components/YarntypeList.js
+++ client/src/components/YarntypeList.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import YarntypeCard from './YarntypeCard';
 
 const YarntypeList = props => {
     return (
         <>
-            <p>Hello, this is the YarntypeList</p>
+            <p>My Yarn</p>
             {props.yarntypes.map((yarntype, index) => {
                 return (
-                    <p key={index}>{yarntype.name}</p>
+                    <YarntypeCard 
+                        key={yarntype.id}
+                        brand={yarntype.brand}
+                        cyc_weight_category={yarntype.cyc_weight_category}
+                        grams={yarntype.grams}
+                        meters={yarntype.meters}
+                        name={yarntype.name}
+                        notes={yarntype.notes}
+                        ounces={yarntype.ounces}
+                        yards={yarntype.yards}
+                    />
                 )
             })}
         </>
